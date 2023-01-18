f15461ee325bbc84b7ca74c2642bb1066885c3c0
soyoung <ouzz1207@gmail.com>
2023-01-18 18:31:24 +0900

    set 정리

--- day05.py
+++ day05.py
@@ -1,34 +1,72 @@
+  # set
 
-# closure
-# closure 사용시 리턴값으로 함수를 받을 수 있다.
-# closure 의 내부함수는, 외부함수로부터 생성된 변수값을 변경하고 저장할 수 있다.
+  # 셋에서 사용하는 함수들
+  # 추가하기  add() , 삭제하기  remove() , 순회하기 for x in set , 멤버십 테스트 in
 
-def calculate():
-  x = 1
-  y = 2
-  temp = 0
-  def add_sub(n): # 내부함수. n을 계속
-    nonlocal temp # 외부에 있는 변수를 변경할 수 있음.
-    temp = temp + x + n - y # 외부함수에 생성된 변수값, 즉 x, y 기억함
-    return temp
-  return add_sub
+  # 일반적으로 사용하는 set의 용도
+drinks = {
+  'martini': {'vodka', 'vermouth'},
+  'black russian': {'vodka', 'kahlua'},
+  'white russian': {'cream', 'kahlua', 'vodka'},
+  'mahattan': {'rye', 'vermouth', 'bitters'},
+  'serewdriver': {'orange juice', 'vodka'}
+}
 
-c1 = calculate()
-for i in range(5):
-  print(i, c1(i))
+for name, contents in drinks.items():
+  if 'vodka' in contents:
+    print(name) # 보드카가 포함된음료 이름 출력
 
+for name, contents in drinks.items():
+  if 'vodka' in contents and not('vermouth' in contents or 'cream' in contents):
+    print(name)
 
-# 연습문제
-# 랜덤으로 1~100중 하나의 정수를 받아서
-# 해당 함수를 제곱하는 함수 만들기
+  # 콤비네이션과 연산자
+  # 셋의 조합 확인하기
 
-def process(no_list, f):
-  for no in no_list:
-    print(f(no))
+# 교집합 &
 
+for name, contents in drinks.items():
+  if 'vodka' in contents and not contents & {'vermouth', 'cream'}: # 샛에담긴 재료중 둘중 하나라도 포함될시 교집합이 존재하므로
+    print(name)
 
-# 랜덤으로 정수 생성
-import random
-numbers = [random.randint(1, 100) for i in range(5)]
-print(numbers)
-process(numbers, lambda x: x ** 2)
+# 두 음료의 재료 셋을 변수에 저장하기
+bruss = drinks['black russian'] # 블랙러시안의 재료(value) 가 저장된다.
+wruss = drinks['white russian']
+
+a = {1, 2}
+b = {2, 3}
+
+# & 연산자 = intersection() method
+print(a & b)
+print(a.intersection(b))
+
+# 음료로 돌아가서
+print(bruss & wruss) # 두 음료에 공통으로 들어가는 재료
+
+# 합집합 구하기. | 또는 union()
+a.union(b)
+
+# 차집합. - 또는 difference()
+
+print(a-b)
+print(a.difference(b)) # b에는 없고 오로지 a에만 있는 것
+
+# 대칭차집합 ^ 또는 symmetric_difference()  (한 쪽 집합에만 속하는 것들의 집합. 두 집합의 합집합 - 교집합)
+print(a ^ b, a.symmetric_difference(b))
+
+# 부분집합인지 판별하기. <= 또는 issubset() 메소드 사용하여 판별, True or False 반환
+
+# 진부분집합인지 판별하기. < 연산자 사용
+
+# 상위집합인지 판별하기. >= 또는 issuperset() 메소드
+
+# 진상위집합 >
+
+## 모든 집합은 자기자신의 부분집합이자 상위집합이다.
+
+# 셋 컴프리헨션
+# 리스트, 딕셔너리 컴프리헨션과 같은 모양.
+a_set = {number for number in range(1, 6) if number % 2 == 0}
+print(a_set)
+
+# 불변 셋 생성하기 : frozenset(순회 가능한 객체 - ex 리스트, 딕셔너리(키값만 저장됨))
