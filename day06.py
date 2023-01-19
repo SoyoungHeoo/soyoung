8f0645afdc85a258eadbe19bdae0bb95f809db11
soyoung <ouzz1207@gmail.com>
2023-01-19 15:09:05 +0900

    예제

--- day06.py
+++ day06.py
@@ -1,83 +1,58 @@
-# Generator
-# yield : 함수를 종료하지 않고 중간중간에 값을 내보낼 수 있다.
-# return : stop iteration 되어 마지막에 값 하나만 내보내게 된다.
-
-
-# decorator
-# 함수를 입력받아 함수를 리턴하는 함수.
-# 함수를 수정하고 싶지만 원본 함수를 건드리고 싶지 않을 때 유용하다.
-
-def document_it(func):  # 데코레이터 함수
-    def new_function(*args, **kwargs):
-        print('Running function: ', func.__name__)
-        print('Positional arguments: ', args)  ## 위치기반 인수
-        print('Keyword arguments: ', kwargs)  ## 키워드 기반 인수
-        result = func(*args, **kwargs)
-        print('Result: ', result)
-        return result
-    return new_function # 클로저 역할?
-
-  # 데커레이터 함수 적용 (수동)
-
-def sub_int(x, y):
-    return x-y
-
-print(sub_int(7, 3))
-info_sub_int = document_it(sub_int)
-r = info_sub_int(7, 3) # 클로저 형식으로 만들어진 데코레이터 동작하는 동안 값들이 print됨.
-print(r) # 리턴된 값 자체도 프린트할 수 있음.
-
-
-# 더 쉬운 사용법
-
-@document_it
-def add_int(x, y):
-    return x+y
-
-add_int(7, 3)
-
-
-# 네임스페이스
-
-# g = 1 # 전역변수. 다른 함수의 내부에서도 접근할 수 있다.
-
-def print_global(): # 함수의 내부는 shade 되어있다. (함수밖에서 접근할 수없음)
-    g = 1
-    print(g)
-
-# print(g)
-
-
-# 재귀함수 (호출하는 함수)
-
-def factorial_iter(n):
+def calc(x, y):
     '''
-    반복문을 사용한 팩토리얼 함수
-    :param n: n!
-    :return: integer 팩토리얼 계산 결과 값
+    나누기 함수
+    :param x:
+    :param y:
+    :return: x / y
     '''
-    result = 1
-    for k in range(1, n+1):
-        result = result * k
-    return result
+    return x / y
 
-print(factorial_iter(5))
+# 파이썬은 모든 에러가 실행에러. (runtime error)
+# 여기에 대한 필연적인 해결책을 만들어두기 위한 것 = try - except 구문
 
-def factorial_recu(n):
+
+
+# try, exception
+
+
+
+#
+
+# 9.2
+
+# while 문 사용
+def get_odds(first=0, last=10):
     '''
-    재귀함수를 사용한 팩토리얼 함수
-    :param n: n!
-    :return: 자기 자신 호출 또는 1
+    입력받은 범위 안에서 홀수를 반환하는 제너레이터함수
+    :param first: integer
+    :param last: integer
+    :return: generator
     '''
-    if n == 1:
-        return n
-    else:
-        return factorial_recu(n-1) * n
+    number = first
+    while number < last:        # 이번 차례의 수가 범위안에 해당할 때
+        if (number % 2 == 1):   # 이번 차례의 수가 홀수라면
+            yield number        # 값 내놓기
+            number += 2         # 2를 더해서 다음 홀수 만들기
+        else:                   # 이번 차례의 수가 홀수가 아니라면
+            number += 1         # 1을 더해서 홀수로 만들기
 
-print(factorial_recu(5))
+gen_odd = get_odds()            # 함수 실행하여 변수에 저장 -> 제너레이터 객체가 gen_odd에 저장된다.
+# gen_odd2 = (i for i in range)
+
+for i in gen_odd:               # for문 돌려 홀수들을 출력한다.
+    print(i)                    # 1, 3, 5, 7, 9 가 차례로 출력될 것이다.
 
 
-# 재귀함수는 리스트의 리스트와 같이 고르지 않은 데이터를 처리할 때 유용하다.
-# flatten
+groups = {
+    '빅뱅': ['GD', '태양', '탑', '대성', '승리'],
+    '마마무': ['솔라', '화사']
+}
 
+
+# 딕셔너리 groups의 value 들의 요소들 뽑아오기
+for i, j in groups.items(): # for문에 key, value 튜플로 받아오는게 핵심
+    for j2 in j :
+        if j2 == '승리':
+            continue
+        print(j2)
 
