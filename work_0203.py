56f8684d06a2b07bdf91d9ca32e4ceb3f3b157d7
soyoung <ouzz1207@gmail.com>
2023-02-03 16:28:41 +0900

    linked list

--- work_0203.py
+++ work_0203.py
@@ -1,30 +1,38 @@
+## 클래스와 함수 선언 부분 ##
+class Node() :
+	def __init__ (self) :
+		self.data = None
+		self.link = None
 
-def add_friend(f_list, name, num):
-    # find_pos = -1
-    for i in range(len(f_list)):
-        if num >= f_list[i][1]:
-            f_list.append(None)
-            # find_pos = i
-            # break
-            for j in range(len(f_list)-1, i, -1):
-                print(f_list[j])
-                f_list[j] = f_list[j-1]
-                f_list[j-1] = None
-            f_list[i] = (name, num)
-            break
+def print_nodes(start) :
+	curr = start
+	if curr == None :
+		return
+	print(curr.data, end = ' ')
+	while curr.link != None: # 다음노드가 비어있지 않으면
+		curr = curr.link # 다음 노드로 이동.
+		print(curr.data, end = ' ') # 다음 데이터 찍음
+	print()
 
+## 전역 변수 선언 부분 ##
+memory = []
+head, curr, pre = None, None, None
+datas = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀"]
 
-# def insert_friend(p, friend)
+## 메인 코드 부분 ##
+if __name__ == "__main__" :
 
-# 전역변수
-friends = [('다현', 200), ('정연', 150), ('쯔위', 90), ('사나', 30), ('지효', 15)]
+	node = Node()		# 첫 번째 노드
+	node.data = datas[0]
+	head = node
+	memory.append(node)
 
-if __name__ == "__main__":
+	for data in datas[1:] :	# 두 번째 이후 노드
+		pre = node
+		node = Node()
+		node.data = data
+		# pre.link = node # 이 부분을 지우면 노드끼리 연결 안됨.
+		memory.append(node)
 
-    print(friends)
-
-    name = input("이름 입력: ")
-    num = int(input("개수 입력: "))
-    add_friend(friends, name, num)
-
-    print(friends)
+	print_nodes(head)
+	print(memory)
