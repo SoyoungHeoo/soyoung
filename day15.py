720ddb250fb8b625fa9def3ed37a67e8f5e68c2d
soyoung <ouzz1207@gmail.com>
2023-02-03 11:45:28 +0900

    delete_data ver0.1, ver0.2

--- day15.py
+++ day15.py
@@ -2,23 +2,25 @@
 pokemons = ['피카츄', '라이츄', '파이리', '꼬부기', '이상해']
 
 
-def insert_data(idx, pokemon):
+def delete_data(idx):
 
     if idx < 0 or idx > len(pokemons):
         print("Out of range!")
         return
 
-    pokemons.append(None)  # 빈칸 추가
+    # for i in range(idx+1, len(pokemons)):
+    #     pokemons[i-1] = pokemons[i]
+    #     pokemons[i] = None
+    #
+    # del(pokemons[len(pokemons)-1])
 
-    for i in range(len(pokemons)-1, idx, -1):
-        pokemons[i] = pokemons[i -1]
-        pokemons[i - 1] = None
+    for i in range(idx, len(pokemons)):
+        del(pokemons[-1])
 
-    pokemons[idx] = pokemon
+    
 
-print(pokemons)
-insert_data(3, '어니부기')
-print(pokemons)
-insert_data(2, '거북왕')
+if __name__ == "__main__":
+    print(pokemons)
+    delete_data(2)
+    print(pokemons)
 
-katok = ['다현', '정연', '쯔위', '사나', '지효']
