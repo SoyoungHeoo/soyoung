861b30c21cba9333d6c42a116bb3a9353f3f8ecc
soyoung <ouzz1207@gmail.com>
2023-02-06 13:37:03 +0900

    circled list

--- day16.py
+++ day16.py
@@ -1,7 +1,7 @@
 
 class Node():
-    def __init__(self):
-        self.data = None
+    def __init__(self, data=None):
+        self.data = data
         self.link = None
 
 def print_nodes(start):
@@ -9,55 +9,60 @@ def print_nodes(start):
     if current == None:
         return
     print(current.data, end=' ')
-    while current.link != None:
+    while current.link != start:
         current = current.link
         print(current.data, end=' ')
     print()
 
-
-
-def insert_data(find, insert):
+def insert_data(find_d, insert_d):
     global memory, head, current, pre
-    if head.data == find:  # 첫 번째 노드 삽입
+    if head.data == find_d:  # 첫 번째 노드 삽입
         node = Node()
-        node.data = insert
+        node.data = insert_d
         node.link = head
+        last = head
+        while last.link != head:
+            last = last.link
+        last.link = node
         head = node
         return
 
     current = head
-    while current.link != None:  # 중간 노드 삽입
+    while current.link != head:  # 중간 노드 삽입
         pre = current
         current = current.link
-        if current.data == find:
+        if current.data == find_d:
             node = Node()
-            node.data = insert
+            node.data = insert_d
             node.link = current
             pre.link = node
             return
 
     node = Node()  # 마지막 노드 삽입
-    node.data = insert
+    node.data = insert_d
     current.link = node
+    node.link = head
 
 
 def delete_nodes(delete_d):
     global memory, head, current, pre
-    if head.data == delete_d:
+    if head.data == delete_d: # 첫번째 노드일 때
         current = head
         head = head.link
-        del current
-        print("첫 노드가 삭제됨")
+        last = head
+        while last.link != current:
+            last = last.link
+        last.link = head
+        del(current)
         return
 
     current = head
-    while current.link != None:
+    while current.link != head:
         pre = current
         current = current.link
         if current.data == delete_d:
             pre.link = current.link
-            del current
-            print("중간노드가 삭제됨")
+            del(current)
             return
     # 삭제할 데이터를 못찾는 경우 함수 종료
     print("삭제할 노드를 찾지 못함")
@@ -68,11 +73,11 @@ def find_nodes(find_data):
     current = head
     if current.data == find_data:
         return current
-    while current.link != None:
+    while current.link != head:
         current = current.link
         if current.data == find_data:
-            return current.data
-    return "Nameless"
+            return current
+    return Node() # 빈 노드 반환
 
 
 def make_linked_list(phone_list):
@@ -101,16 +106,47 @@ def make_linked_list(phone_list):
     # 삽입하는 노드가 가장 큰 경우
     current.link = node
 
+def is_find(find_data):
+    '''
+    연결리스트 안에서 원소 존재 여부 판정 함수
+    :param find_data: 찾고자 하는 원소. str
+    :return: 원소 존재시 True, 아니면 False
+    '''
+    global memory, head, current, pre
+    current = head
+    if current.data == find_data:
+        return True
+    while current.link != head:
+        current = current.link
+        if current.data == find_data:
+            return True
+    return False # 빈 노드 반환
+
+
 
 memory = []
 head, current, pre = None, None, None
-data_array = [["지민", 180], ["정국", 177], ["뷔", 183], ["슈가", 175], ["진", 179]]
+# data_array = [["지민", 180], ["정국", 177], ["뷔", 183], ["슈가", 175], ["진", 179]]
+data_array = ['피카츄', '파이리', '어니부기', '꼬부기', '버터풀']
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__" :
 
-    for data in data_array :	# 두 번째 이후 노드
-        make_linked_list(data) # 한개씩 데이터르 넣어 알맞는 위치에 배치해줍니다
+    node = Node()
+    node.data = data_array[0]
+    head = node
+    node.link = head
+
+    for data in data_array[1:] :	# 두 번째 이후 노드
+        # make_linked_list(data) # 한개씩 데이터르 넣어 알맞는 위치에 배치해줍니다
+        pre = node
+        node = Node(data)
+        pre.link = node
+        node.link = head
+
+    print(is_find("꼬부기"))
+    print(is_find("피카츄"))
+    print(is_find("김인하"))
 
     print_nodes(head)
     #
