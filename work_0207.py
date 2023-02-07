090ddb61ca42304296c700238156d6593ea0f3c8
soyoung <ouzz1207@gmail.com>
2023-02-07 18:55:07 +0900

    8장 응용예제1 - 편의점에서 판매된 물건 출력하기

--- work_0207.py
+++ work_0207.py
@@ -1,76 +1,49 @@
-# 7장 응용예제 2 - 콜센터 응답 대기 시간 계산하기
+# 8장 응용예제 1 - 편의점에서 판매된 물건 목록 출력하기
+import random
 
-def is_queue_full():
-    global front, rear, SIZE
-    if front == (rear+1) % SIZE:
-        return True
-    else:
-        return False
+# 노드 만들기
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.right = None
+        self.left = None
 
+# 전역변수
+memory = list()
+items = ['캔커피', '도시락', '삼각김밥', '츄파츕스', '코카콜라']
+sold_items = [random.choice(items) for _ in range(20)]
 
-def is_queue_empty():
-    global front, rear, SIZE
-    if front == rear:
-        return True
-    else:
-        return False
-
-
-def add_waiting(data):
-    global WAITING_TIME, front, rear, SIZE
-    if is_queue_full():
-        print("Queue is Empty")
-        return
-    rear = (rear + 1) % SIZE
-    queue_line[rear] = data
-    calc_waiting_time(data[1])
-    return
-
-
-def finish_waiting():
-    global WAITING_TIME, front, rear
-    if is_queue_empty():
-        print("Queue is Full!")
-        return
-    front = (front+1) % SIZE
-    data = queue_line[front]
-    queue_line[front] = None
-    calc_waiting_time(-1 * data[1])
-    return data
-
-
-def calc_waiting_time(time_int):
-    global WAITING_TIME
-    WAITING_TIME += time_int
-
-
-def peek():
-    global WAITING_TIME, front, rear
-    if is_queue_empty():
-        return("Queue is Empty")
-    return queue_line[front+1]
-
-
-WAITING_TIME = 0
-front = rear = -1
-SIZE = 5
-queue_line = [None for _ in range(SIZE)]
-
-
+# 메인함수
 if __name__ == "__main__":
-    add_waiting(('사용',9))
-    print(f'')
-    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
-    add_waiting(('고장',3))
-    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
-    add_waiting(('환불',4))
-    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
-    add_waiting(('고장',3))
-    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
-    print(queue_line)
-    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
+    # root
+    node = Node()
+    node.data = sold_items[0]
+    root = node
+    for item in sold_items[1:]:
+        node = Node(item)
+        current = root
+        while True:
+            if item == current.data:
+                break
+            if item < current.data :
+                if current.left == None:
+                    current.left = node
+                    break
+                current = current.left
+            else:
+                if current.right == None:
+                    current.right = node
+                    break
+                current = current.right
 
-    finish_waiting()
-    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
-    print(queue_line)
+
+    def preorder(node):
+        if node == None:
+            return
+        print(node.data, end = ' ')
+        preorder(node.left)
+        preorder(node.right)
+
+    print(sold_items)
+    preorder(root)
 
