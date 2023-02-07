0cd605cc3a4f068d93e6d7299edb33640e53b4b3
soyoung <ouzz1207@gmail.com>
2023-02-07 18:19:09 +0900

    # 7장 응용예제 2 - 콜센터 응답 대기 시간 계산하기

--- work_0207.py
+++ work_0207.py
@@ -1,50 +1,76 @@
-# 7장 큐 응용예제 1 - 유명 맛집의 대기줄 구현하기
+# 7장 응용예제 2 - 콜센터 응답 대기 시간 계산하기
 
 def is_queue_full():
-    global rear, front, SIZE
-    if rear == (SIZE - 1):
+    global front, rear, SIZE
+    if front == (rear+1) % SIZE:
         return True
     else:
         return False
 
+
 def is_queue_empty():
-    global rear, front
+    global front, rear, SIZE
     if front == rear:
         return True
     else:
         return False
 
-def add_guest(guest):
-    global rear, front
-    rear = rear + 1
-    waiting[rear] = guest
 
-def delete_guest():
-    global rear, front
-    front = front + 1
-    data = waiting[front]
-    waiting[front] = None
-    for i in range(front+1, rear+1):
-        waiting[i-1] = waiting[i]
-        waiting[i] = None
-    front -= 1
-    rear -= 1
+def add_waiting(data):
+    global WAITING_TIME, front, rear, SIZE
+    if is_queue_full():
+        print("Queue is Empty")
+        return
+    rear = (rear + 1) % SIZE
+    queue_line[rear] = data
+    calc_waiting_time(data[1])
+    return
 
+
+def finish_waiting():
+    global WAITING_TIME, front, rear
+    if is_queue_empty():
+        print("Queue is Full!")
+        return
+    front = (front+1) % SIZE
+    data = queue_line[front]
+    queue_line[front] = None
+    calc_waiting_time(-1 * data[1])
     return data
 
-# 전역변수
+
+def calc_waiting_time(time_int):
+    global WAITING_TIME
+    WAITING_TIME += time_int
+
+
+def peek():
+    global WAITING_TIME, front, rear
+    if is_queue_empty():
+        return("Queue is Empty")
+    return queue_line[front+1]
+
+
+WAITING_TIME = 0
 front = rear = -1
 SIZE = 5
-waiting = [None for _ in range(SIZE)]
+queue_line = [None for _ in range(SIZE)]
 
-# 메인코드
+
 if __name__ == "__main__":
-    add_guest("정국")
-    add_guest("뷔")
-    add_guest("지민")
-    add_guest("슈가")
-    print(f'대기줄 상태 : {waiting}')
+    add_waiting(('사용',9))
+    print(f'')
+    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
+    add_waiting(('고장',3))
+    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
+    add_waiting(('환불',4))
+    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
+    add_waiting(('고장',3))
+    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
+    print(queue_line)
+    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
 
-    for _ in range(rear+1):
-        print(delete_guest(), " 님 입장")
-        print(f'대기줄 상태 : {waiting}')
+    finish_waiting()
+    print(f'귀하의 대기시간은 {WAITING_TIME}분 입니다.')
+    print(queue_line)
+
