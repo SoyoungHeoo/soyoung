a79bd6fea75a65d66f098b50ac33b05b5d96e277
soyoung <ouzz1207@gmail.com>
2023-02-09 23:45:01 +0900

    재귀호출 응용예제 2 - 시에르핀스키 삼각형 그리기

--- work_0209.py
+++ work_0209.py
@@ -1,15 +1,25 @@
-def notation(base, n):
-    if n < base :
-        print(numberChar[n], end='')
+import tkinter as tk
+
+
+def sierpinski(x, y, size):
+    if size>=30:
+        sierpinski(x, y, size/2)
+        sierpinski(x+size/2, y, size/2)
+        sierpinski(x+size/4, int(y-size*(3**0.5)/4), size/2)
     else:
-        notation(base, n // base)
-        print(numberChar[n % base], end='')
+        canvas.create_polygon(x, y, x+size, y, x+size/2, y-size*(3**0.5)/2,
+                              fill='red', outline='red')
 
 
-numberChar = [str(i) for i in range(10)]
-numberChar += ['A', 'B', 'C', 'D', 'E', 'F']
-
+w = 500
+radius = 500
 
 if __name__ == "__main__":
-    number = 4
-    notation(2, number)
+    window = tk.Tk()
+    window.title("삼각형 모양의 프렉탈")
+    canvas = tk.Canvas(window, height=w, width=w, bg="white")
+
+    sierpinski(40, 400, 300)
+
+    canvas.pack()
+    window.mainloop()
